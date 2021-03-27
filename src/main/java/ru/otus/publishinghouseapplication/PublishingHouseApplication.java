2f77fabd1dd10d33f84740003dc321cac418b2b2
Azat Fazlyev <fazliev14@gmail.com>
2021-03-27 19:51:40 +0300

    Initial commit

---
+++ src/main/java/ru/otus/publishinghouseapplication/PublishingHouseApplication.java
@@ -0,0 +1,11 @@
+package ru.otus.publishinghouseapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublishingHouseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PublishingHouseApplication.class, args);
+    }
+}
